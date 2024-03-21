eb1d5792286afbbb83874ff96a6192949bfecaa7
Sabshor <lsabshor@mail.ru>
2024-03-21 16:31:23 +0300

    add main method and call cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+    //@CommandLine.Option(names = "-x") int x;
+    @Override
+    public Integer call() { // business logic
+        var diff = Differ.generate("", "");
+        //System.out.printf("x=%s%n", x);
+        //return 123; // exit code
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.exit(new picocli.CommandLine(new App()).execute(args));
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public class Differ {
+    public static String generate(String filePath1, String filePath2) {
+        System.out.println("Hello differ");
+        return "OK";
+    }
+}
