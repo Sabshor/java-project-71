969dc6340ac1d99909002455a6b8eaa524f5f526
Sabshor <lsabshor@mail.ru>
2024-04-10 15:03:59 +0300

    add unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,10 +19,14 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.32")
 
     implementation("info.picocli:picocli:4.7.5")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
 
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    //testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    //testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
