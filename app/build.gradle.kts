03ef5fef510158c0fde746e55850d07f95e8cac6
Sabshor <lsabshor@mail.ru>
2024-04-15 13:10:41 +0300

    add commons-io dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,10 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
 
+    implementation("commons-io:commons-io:2.16.1")
     implementation("info.picocli:picocli:4.7.5")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
